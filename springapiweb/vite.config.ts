54270c38d0bd0b09b4983b883000d19cfafe3276
Rafael Briet <rafael-briet@hotmail.com>
2025-08-21 10:12:08 -0300

    Add docker support

--- springapiweb/vite.config.ts
+++ springapiweb/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/veiculos': {
-        target: 'http://localhost:5000',
+        target: process.env.VITE_API_BASE_URL || 'http://localhost:8080',
         changeOrigin: true,
       },
     },
